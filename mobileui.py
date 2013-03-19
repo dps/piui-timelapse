84c971d4ea43a1b7263e36589ba1994705468481
David Singleton <davidsingleton@gmail.com>
2013-03-18 20:40:47 -0700

    cleaned up

---
+++ mobileui.py
@@ -0,0 +1,108 @@
+import os
+import time
+
+from piui import PiUi
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+class TimelapsePiUi(object):
+
+  def __init__(self, camera, idy, convert):
+    self._ui = PiUi(img_dir=os.path.join(current_dir))
+    self._camera = camera
+    self._idy = idy
+    self._convert = convert
+
+  def thumbname(self, filename):
+    return filename[:filename.rfind('.')] + "_thumb" + filename[filename.rfind('.'):]
+
+  def show_config(self, configs, current):
+    config = self._configs[self._current_config]
+    self.exp.set_text("T: %s ISO: %d" % (config[0], config[1]))
+
+  def show_status(self, shot, configs, current):
+    config = self._configs[self._current_config]
+    self.title.set_text("Shot %d" % shot)
+    self.exp.set_text("T: %s ISO: %d" % (config[0], config[1]))
+
+  def should_stop(self):
+    return self._stop
+
+  def show_error(self, err):
+    self.title.set_text(err)
+
+  def ondownclick(self):
+    self._current_config -= 1
+    if self._current_config < 0:
+      self._current_config = 0
+    self.show_config(self._configs, self._current_config)
+
+  def onupclick(self):
+    self._current_config += 1
+    if self._current_config > len(self._configs):
+      self._current_config = len(self._configs) - 1
+    self.show_config(self._configs, self._current_config)
+
+  def onstartclick(self):
+    self._done = True
+
+  def onstopclick(self):
+    self.title.set_text("Stopping...")
+    self._stop = True
+
+  def onpreviewclick(self):
+    self.title.set_text("Capturing Preview...")
+    config = self._configs[self._current_config]
+    try:
+      self._camera.set_shutter_speed(secs=config[0])
+      self._camera.set_iso(iso=str(config[1]))
+    except Exception, e:
+      self.title.set_text(str(e))
+    filename = None
+    brightness = 0.0
+    try:
+      filename = self._camera.capture_image_and_download()
+    except Exception, e:
+      self.title.set_text(str(e))
+    if filename:
+      brightness = float(self._idy.mean_brightness(filename))
+    self._convert.thumbnail(filename, self.thumbname(filename))
+    filename = self.thumbname(filename)
+    self.img.set_src(filename[filename.rfind('/')+1:])
+    self.title.set_text("Preview: %f [25k tgt]" % brightness)
+    
+  def new_frame(self, filename, brightness):
+    self._convert.thumbnail(filename, self.thumbname(filename))
+    filename = self.thumbname(filename)
+    self.img.set_src(filename[filename.rfind('/')+1:])
+    self.bright.set_text("br: %f" % brightness)
+
+
+  def main(self, configs, current_config, network_status):
+    self._configs = configs
+    self._current_config = current_config
+    self.page = self._ui.new_ui_page(title="Timelapse")
+    self.title = self.page.add_textbox("Initial Exposure", "h3")
+    self.exp = self.page.add_textbox("", "h2")
+    plus = self.page.add_button("--&uarr;--", self.onupclick)
+    minus = self.page.add_button("--&darr;--", self.ondownclick)
+    preview = self.page.add_button("Preview", self.onpreviewclick)
+    start = self.page.add_button("Start", self.onstartclick)
+    self.page.add_element('br')
+    self.img = self.page.add_image("")
+    self._done = False
+    self.show_config(self._configs, self._current_config)
+    while not self._done:
+        time.sleep(1)
+    self.page = self._ui.new_ui_page(title="Shooting")
+    self.title = self.page.add_textbox("", "h2")
+    self.exp = self.page.add_textbox("", "h3")
+    stop = self.page.add_button("Stop", self.onstopclick)
+    self.page.add_element('br')
+    self.img = self.page.add_image("sunset.png")
+    self.page.add_element("br")
+    self.bright = self.page.add_textbox("", "p")
+    self._stop = False
+    return self._current_config
+
---
+++ tl.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python
+
+from datetime import datetime
+from datetime import timedelta
+import subprocess
+import time
+
+from wrappers import GPhoto
+from wrappers import Convert
+from wrappers import Identify
+from wrappers import NetworkInfo
+
+from mobileui import TimelapsePiUi
+
+MIN_INTER_SHOT_DELAY_SECONDS = timedelta(seconds=30)
+MIN_BRIGHTNESS = 20000
+MAX_BRIGHTNESS = 30000
+
+CONFIGS = [("1/1600", 200),
+           ("1/1000", 200),
+           ("1/800", 200),
+           ("1/500", 200),
+           ("1/320", 200),
+           ("1/250", 200),
+           ("1/200", 200),
+           ("1/160", 200),
+           ("1/100", 200),
+           ("1/80", 200),
+           ("1/60", 200),
+           ("1/50", 200),
+           ("1/40", 200),
+           ("1/30", 200),
+           ("1/20", 200),
+           ("1/15", 200),
+           ("1/13", 200),
+           ("1/10", 200),
+           ("1/6", 200),
+           ("1/5", 200),
+           ("1/4", 200),
+           ("0.3", 200),
+           ("0.5", 200),
+           ("0.6", 200),
+           ("0.8", 200),
+           ("1", 200),
+           ("1.6", 200),
+           ("2.5", 200),
+           ("3.2", 200),
+           ("5", 200),
+           ("8", 200),
+           ("10", 200),
+           ("13", 200),
+           ("15", 200),
+           ("20", 200),
+           ("30", 200),
+           ("30", 400),
+           ("30", 800),
+           ("30", 1600)]
+
+def test_configs():
+    camera = GPhoto(subprocess)
+
+    for config in CONFIGS:
+      camera.set_shutter_speed(secs=config[0])
+      camera.set_iso(iso=str(config[1]))
+      time.sleep(1)
+
+def main():
+    #print "Testing Configs"
+    #test_configs()
+    print "Timelapse"
+    camera = GPhoto(subprocess)
+    idy = Identify(subprocess)
+    netinfo = NetworkInfo(subprocess)
+    convert = Convert(subprocess)
+
+    while True:
+      ui = TimelapsePiUi(camera, idy, convert)
+      
+      current_config = 11
+      shot = 0
+      prev_acquired = None
+      last_acquired = None
+      last_started = None
+
+      network_status = netinfo.network_status()
+      current_config = ui.main(CONFIGS, current_config, network_status)
+
+      try:
+          while True:
+              last_started = datetime.now()
+              config = CONFIGS[current_config]
+              print "Shot: %d Shutter: %s ISO: %d" % (shot, config[0], config[1])
+              ui.show_status(shot, CONFIGS, current_config)
+              camera.set_shutter_speed(secs=config[0])
+              camera.set_iso(iso=str(config[1]))
+              try:
+                filename = camera.capture_image_and_download()
+              except Exception, e:
+                print "Error on capture." + str(e)
+                print "Retrying..."
+                # Occasionally, capture can fail but retries will be successful.
+                continue
+              prev_acquired = last_acquired
+              brightness = float(idy.mean_brightness(filename))
+              last_acquired = datetime.now()
+
+              ui.new_frame(filename, brightness)
+
+              print "-> %s %s" % (filename, brightness)
+
+              if brightness < MIN_BRIGHTNESS and current_config < len(CONFIGS) - 1:
+                  current_config = current_config + 1
+              elif brightness > MAX_BRIGHTNESS and current_config > 0:
+                  current_config = current_config - 1
+              else:
+                  if last_started and last_acquired and last_acquired - last_started < MIN_INTER_SHOT_DELAY_SECONDS:
+                      print "Sleeping for %s" % str(MIN_INTER_SHOT_DELAY_SECONDS - (last_acquired - last_started))
+
+                      time.sleep((MIN_INTER_SHOT_DELAY_SECONDS - (last_acquired - last_started)).seconds)
+              shot = shot + 1
+              if ui.should_stop():
+                break
+      except Exception,e:
+          ui.show_error(str(e))
+
+
+if __name__ == "__main__":
+    main()
